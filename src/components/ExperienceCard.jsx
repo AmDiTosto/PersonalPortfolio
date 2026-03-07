d16bd6f57846aab0d76b272f83e89b8866ee62cc
Adrian Di Tosto <aditosto@uoguelph.ca>
2026-03-07 16:21:47 -0500

    updated componenet to have icons with links

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,8 +1,8 @@
 export default function ExperienceCard({ exp }) {
   return (
     <div className="w-full p-4 border-2 border-t-white border-l-white border-r-[#808080] border-b-[#808080] bg-[#c3c7cb]">
-      <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
-        <div className="shrink-0 sm:w-[220px] border-2 border-t-white border-l-white border-r-[#808080] border-b-[#808080] p-2">
+      <div className="flex flex-col gap-4 ">
+        <div className=" border-2 border-t-white border-l-white border-r-[#808080] border-b-[#808080] p-2">
           <div className="flex items-center justify-center border-2 border-t-[#808080] border-l-[#808080] border-r-white border-b-white bg-[#c3c7cb] h-[200px]">
             <img
               src={exp.icon}
@@ -13,15 +13,43 @@ export default function ExperienceCard({ exp }) {
         </div>
 
         <div className="flex-1">
-          <div className="font-fixedsys text-[18px] text-[#24415f] truncate">
-            {exp.role}
+          <div className="flex items-center justify-start gap-x-4 font-fixedsys text-xl text-[#24415f] truncate">
+            <span>{exp.role}</span>
+
+            {exp.links?.length ? (
+              <div className="flex items-center gap-3">
+                {exp.links.map((l) => (
+                  <a
+                    key={l.href}
+                    href={l.href}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="
+                      font-fixedsys text-sm text-[#0000aa]
+                      underline
+                      hover:text-[#000088]
+                    "
+                  >
+                    <img
+                      src={l.icon}
+                      alt={`${l.icon} logo`}
+                      className="h-[30px] w-[30px]"
+                    />
+                  </a>
+                ))}
+              </div>
+            ) : null}
           </div>
 
-          <div className=" font-fixedsys text-sm text-[#24415f]">
-            {exp.start} — {exp.end}
+          <div className=" font-fixedsys text-md text-[#24415f]">
+            {exp.start && exp.end && (
+              <>
+                {exp.start} — {exp.end}
+              </>
+            )}
           </div>
 
-          <div className="font-fixedsys text-sm text-[#24415f]">
+          <div className="font-fixedsys text-md text-[#24415f]">
             <span className="truncate">{exp.company}</span>
           </div>
 
@@ -31,11 +59,11 @@ export default function ExperienceCard({ exp }) {
           </div>
 
           {exp.highlights?.length ? (
-            <ul className="mt-2 list-disc space-y-1 pl-5 font-fixedsys text-sm text-[#24415f]">
+            <div className="space-y-1 font-fixedsys text-md text-[#24415f]">
               {exp.highlights.map((h, i) => (
-                <li key={i}>{h}</li>
+                <p key={i}>{h}</p>
               ))}
-            </ul>
+            </div>
           ) : null}
 
           {exp.tech?.length ? (
@@ -43,30 +71,10 @@ export default function ExperienceCard({ exp }) {
               {exp.tech.map((t) => (
                 <span
                   key={t}
-                  className="border border-[#808080] bg-[#c0c0c0] px-2 py-[2px] font-fixedsys text-xs text-[#24415f]"
+                  className="border border-[#808080] bg-[#c0c0c0] px-2 py-[2px] font-fixedsys text-sm text-[#24415f]"
                 >
                   {t}
                 </span>
-              ))}
-            </div>
-          ) : null}
-
-          {exp.links?.length ? (
-            <div className="mt-4 flex flex-wrap gap-3">
-              {exp.links.map((l) => (
-                <a
-                  key={l.href}
-                  href={l.href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="
-                      font-fixedsys text-sm text-[#0000aa]
-                      underline
-                      hover:text-[#000088]
-                    "
-                >
-                  {l.label}
-                </a>
               ))}
             </div>
           ) : null}
